6d3fe7faa89ff922b3d092226356af70890f823a
Rodrigo Barrera <srok@srok.com.ar>
2017-12-20 14:28:57 -0300

    ahora para windows

--- www/js/app.js
+++ www/js/app.js
@@ -264,11 +264,18 @@ $rootScope.showAlert = function() {
 
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+    // if (window.cordova && window.cordova.plugins.Keyboard) {
+    //   cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    //   cordova.plugins.Keyboard.disableScroll(true);
 
+    // }
+    if( window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard ) {
+    // Check reference to avoid runtime error on windows phone
+    if( window.cordova.plugins.Keyboard.hideKeyboardAccessoryBar ) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar( true );
     }
+    cordova.plugins.Keyboard.disableScroll( true );
+}
     if (window.StatusBar) { 
       // org.apache.cordova.statusbar required
       StatusBar.styleDefault();
